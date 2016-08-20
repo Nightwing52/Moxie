7fe1305cee9eeb95d3b2ea0a72807aa73bc84a45
Daniel Holman <coderdan657@gmail.com>
2016-08-19 19:37:24 -0500

    Added keysym hooks to the Input class
    Added keysym check to Input::Poll()

--- Framework/Input.cpp
+++ Framework/Input.cpp
@@ -5,5 +5,7 @@ Input::Input() {
 
 int Input::Poll(void) {
         SDL_PollEvent(&event);
+        if (event.type == SDL_KEYDOWN)
+                return event.key.keysym.sym;
         return event.type;
 }
