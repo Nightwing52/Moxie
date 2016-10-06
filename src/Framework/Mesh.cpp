4612e90fca8651e8e232381b9aa75e5484733be9
Daniel Holman <coderdan657@gmail.com>
2016-10-05 21:20:53 -0500

    Added OpenGL code
    Added OpenGL buffer initialization to the Mesh class

--- src/Framework/Mesh.cpp
+++ src/Framework/Mesh.cpp
@@ -45,11 +45,52 @@ Mesh::Mesh(const string &filename) {
                 }
         }
         file.close();
+
+        unsigned int sizeVerts = sizeof(float) * 3 * sizeof(m_vertices);
+        unsigned int sizeTexCs = sizeof(float) * 2 * sizeof(m_texCoords);
+        unsigned int sizeNorms = sizeof(float) * 3 * sizeof(m_normals);
+        unsigned int sizeIndxs = sizeof(float) * 3 * sizeof(m_indices);
+
+        // Needs testing with Moxie format
+        glGenVertexArrays(1, &m_VAO);
+        glBindVertexArray(m_VAO);
+        glGenBuffers(NUM_BUFFERS, m_VABs);
+
+        glBindBuffer(GL_ARRAY_BUFFER, m_VABs[POSITION_VB]);
+        glBufferData(GL_ARRAY_BUFFER, sizeVerts, m_vertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, m_VABs[TEXCOORD_VB]);
+        glBufferData(GL_ARRAY_BUFFER, sizeTexCs, m_texCoords, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, m_VABs[NORMAL_VB]);
+        glBufferData(GL_ARRAY_BUFFER, sizeNorms, m_normals, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_VABs[INDEX_VB]);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeIndxs, m_indices,
+                        GL_STATIC_DRAW);
+
+        glBindVertexArray(0);
 }
 
 void Mesh::Draw(const float x, const float y, const float z) {
         glm::vec3 pos(x, y, z);
+        pos.x = x;
+        pos.y = y;
+        pos.z = z;
+
+        glBindVertexArray(m_VAO);
+        glDrawElementsBaseVertex(GL_TRIANGLES, sizeof(m_indices),
+                        GL_UNSIGNED_INT, 0, 0);
+        glBindVertexArray(0);
 }
 
 Mesh::~Mesh() {
+        glDeleteBuffers(NUM_BUFFERS, m_VABs);
+        glDeleteVertexArrays(1, &m_VAO);
 }
